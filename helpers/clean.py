39cdcb3cc34e4b91e5b23f6c29901dab5bc4d5b3
asbhaibsr <ffloverisarsad@gmail.com>
2025-06-18 21:43:25 +0530

    Create clean.py

---
+++ helpers/clean.py
@@ -0,0 +1,12 @@
+import os
+
+
+def clean_temp():
+    path = "downloads"
+    for file_name in os.listdir(path):
+        file_path = os.path.join(path, file_name)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f"Error deleting file {file_path}: {e}")
